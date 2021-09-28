7b88c14b7ad77ac88e3fee6cde3b768ebc8b5792
Zuzia780 <80356801+Zuzia780@users.noreply.github.com>
2021-09-29 01:16:29 +0200

    Update checkIfPrime.java

--- checkIfPrime.java
+++ checkIfPrime.java
@@ -6,25 +6,26 @@ class checkIfPrime {
 
 	@Test
 void oneAndTwo() {
+	void oneAndTwo() {
 		Tasks1to5 test = new Tasks1to5();
 		boolean two = test.checkIfPrime(2);
 		assertEquals(true,two);
 		boolean one = test.checkIfPrime(1);
-		assertEquals(false,one);
+		assertFalse(one);
 	}
 	void prime() {
 		Tasks1to5 test = new Tasks1to5();
 		boolean seven = test.checkIfPrime(7);
-		assertEquals(true,seven);
+		assertTrue(seven);
 	}
 	void test3() {
 		Tasks1to5 test = new Tasks1to5();
 		boolean even= test.checkIfPrime(12);
-		assertEquals(false,even);
+		assertFalse(even);
 	}
 	void test4() {
 		Tasks1to5 test = new Tasks1to5();
 		boolean one = test.checkIfPrime(1);
-		assertEquals(false,one);
+		assertFalse(one);
 	}
-}
+
