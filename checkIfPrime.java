aefadb223eb18aa5cb5c1acb0c5d8f8ecc3f7134
Zuzia780 <80356801+Zuzia780@users.noreply.github.com>
2021-09-29 01:00:24 +0200

    Update checkIfPrime.java

--- checkIfPrime.java
+++ checkIfPrime.java
@@ -5,16 +5,26 @@ import org.junit.jupiter.api.Test;
 class checkIfPrime {
 
 	@Test
-	void test() {
-		Task1 test = new Task1();
+void oneAndTwo() {
+		Tasks1to5 test = new Tasks1to5();
 		boolean two = test.checkIfPrime(2);
 		assertEquals(true,two);
-		boolean primeNumber = test.checkIfPrime(7);
-		assertEquals(true,primeNumber);
-		boolean even= test.checkIfPrime(12);
-		assertEquals(false,even);
 		boolean one = test.checkIfPrime(1);
 		assertEquals(false,one);
 	}
-
+	void prime() {
+		Tasks1to5 test = new Tasks1to5();
+		boolean seven = test.checkIfPrime(7);
+		assertEquals(true,seven);
+	}
+	void test3() {
+		Tasks1to5 test = new Tasks1to5();
+		boolean even= test.checkIfPrime(12);
+		assertEquals(false,even);
+	}
+	void test4() {
+		Tasks1to5 test = new Tasks1to5();
+		boolean one = test.checkIfPrime(1);
+		assertEquals(false,one);
+	}
 }
