83656df1f07e9bd2fdaa991eae17c88233ace77c
Zuzia780 <80356801+Zuzia780@users.noreply.github.com>
2021-09-29 01:03:08 +0200

    Update reverseString.java

--- reverseString.java
+++ reverseString.java
@@ -6,11 +6,17 @@ class reverseString {
 
 	@Test
 	void test() {
-		Task1 test= new Task1();
+		Tasks1to5 test= new Tasks1to5();
 		String word= test.reverseString("word");
 		assertEquals("drow", word);
+	}
+	void sentence() {
+		Tasks1to5 test= new Tasks1to5();
 		String sentence=test.reverseString("Hello world");
 		assertEquals("dlrow olleH", sentence);
+	}
+	void empty() {
+		Tasks1to5 test= new Tasks1to5();
 		String empty = test.reverseString("");
 		assertEquals("",empty);
 	}
