295e3b49e6c0612a6fe5e1d31bef7738e7619173
Zuzia780 <80356801+Zuzia780@users.noreply.github.com>
2021-09-29 01:01:55 +0200

    Update countPrimeFactors.java

--- countPrimeFactors.java
+++ countPrimeFactors.java
@@ -3,17 +3,28 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class countPrimeFactors {
-	void test(){
-	Task1 test= new Task1();
-	int even= test.countPrimeFactors(10);
-	assertEquals(2,even);
-	int one= test.countPrimeFactors(1);
-	assertEquals(0,one);
-	int two=test.countPrimeFactors(2);
-	assertEquals(1,two);
-	int prime=test.countPrimeFactors(11);
-	assertEquals(1,prime);
+	void test() {
+		Tasks1to5 test= new Tasks1to5();
+		int even= test.countPrimeFactors(10);
+		assertEquals(2,even);
 	}
+	void one() {
+		Tasks1to5 test= new Tasks1to5();
+		int one= test.countPrimeFactors(1);
+		assertEquals(0,one);
+	}
+	void two() {
+		Tasks1to5 test= new Tasks1to5();
+		int two=test.countPrimeFactors(2);
+		assertEquals(1,two);
+	}
+	void prime(){
+		Tasks1to5 test= new Tasks1to5();
+		int prime=test.countPrimeFactors(11);
+		assertEquals(1,prime);
+	}
+	
+
 	
 	
 
