8b967b187b3c65d6f68df03695d5df4ba7bd41ee
Zuzia780 <80356801+Zuzia780@users.noreply.github.com>
2021-09-29 01:02:40 +0200

    Update divide.java

--- divide.java
+++ divide.java
@@ -5,14 +5,27 @@ import org.junit.jupiter.api.Test;
 class divide {
 
 	@Test
-	void test() {
-		Task1 test = new Task1();
+	void divisorZero() {
+		Tasks1to5 test = new Tasks1to5();
 		double divisorZero = test.divide(5, 0);
-		assertEquals(0, divisorZero);
-		double divisorSmaller = test.divide(5,2);
-		assertEquals(2.5,divisorSmaller);
-		double dividendZero = test.divide(0, 5);
-		assertEquals(0,dividendZero);
+		assertEquals(0, divisorZero);	
 	}
+	void divisorSmaller() {
+		Tasks1to5 test = new Tasks1to5();
+		double fiveAnd2 = test.divide(5,2);
+		assertEquals(2.5,fiveAnd2);
+	}
+	void dividentSmaller() {
+		Tasks1to5 test=new Tasks1to5();
+		double twoAnd5 = test.divide(2, 5);
+		assertEquals(0.4, twoAnd5);
+	}
+	void dividendZero() {
+		Tasks1to5 test = new Tasks1to5();
+		double five = test.divide(0, 5);
+		assertEquals(0,five);
+	}
+
+}
 
 }
