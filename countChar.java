9c73e211811b8a6257ce1a9353b559e19eed3faa
Zuzia780 <80356801+Zuzia780@users.noreply.github.com>
2021-09-29 01:01:00 +0200

    Update countChar.java

--- countChar.java
+++ countChar.java
@@ -6,16 +6,25 @@ class countChar {
 
 	@Test
 	void test() {
-		Task1 test = new Task1();
+		void word() {
+		Tasks1to5 test = new Tasks1to5();
 		int word = test.countChar("window", 'w');
 		assertEquals(2,word);
+	}
+	void noChar() {
+		Tasks1to5 test = new Tasks1to5();
 		int wordZero=test.countChar("twobeersarebetterthanone", 'z');
 		assertEquals(0, wordZero);
+	}
+	void sentence() {
+		Tasks1to5 test = new Tasks1to5();
 		int sentence = test.countChar("Hello word", 'o');
 		assertEquals(2, sentence);
+	}
+	void spaces() {
+		Tasks1to5 test = new Tasks1to5();
 		int spaces = test.countChar("Jingle bells is it halloween?",' ');
 		assertEquals(4,spaces);
-				
-		}
-
+	}
+	
 }
