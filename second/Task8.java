6331129c8952effc165179a546cfc49497dfaa8d
Zuzia780 <80356801+Zuzia780@users.noreply.github.com>
2021-10-21 22:19:31 +0200

    Add files via upload

---
+++ second/Task8.java
@@ -0,0 +1,31 @@
+package second;
+public class Task8 {
+//task 8
+	int checkWhoWon (char a, char b) {
+		if(a==b) return 0;
+		if (a=='K') {
+			if(b=='P') 
+				return -1;
+			else return 1;
+			}
+		if (a=='P') {
+			if (b=='K')
+				return 1;
+			else return -1;
+			}
+		if (a=='N') {
+			if (b=='K')
+				return -1;
+			else  return 1;
+			}
+		return 0;
+		}
+	int countDeficit (String a, String b) {
+		int counta=0, countb=0, result=0;
+		for(int i=0;i<a.length();i++) {
+			result=checkWhoWon(a.charAt(i),b.charAt(i));
+			if (result==1) counta++;
+			if(result==-1) countb++;		}
+		return Math.abs(counta-countb);
+	}
+}
