58d09e3f802b918470bcf1836c917562d7a8b3cd
Zuzia780 <80356801+Zuzia780@users.noreply.github.com>
2021-10-21 22:04:16 +0200

    Update Part2.java

--- second/Part2.java
+++ second/Part2.java
@@ -2,26 +2,21 @@ package second;
 public class Part2 {
 //task 8
 	int checkWhoWon (char a, char b) {
+		if(a==b) return 0;
 		if (a=='K') {
-			if (b=='K')
-				return 0;
-			else if(b=='P') 
-				return 2;
+			if(b=='P') 
+				return -1;
 			else return 1;
 			}
 		if (a=='P') {
 			if (b=='K')
 				return 1;
-			else if(b=='P') 
-				return 0;
-			else return 2;
+			else return -1;
 			}
 		if (a=='N') {
 			if (b=='K')
-				return 2;
-			else if(b=='P') 
-				return 1;
-			else return 0;
+				return -1;
+			else  return 1;
 			}
 		return 0;
 		}
@@ -30,7 +25,7 @@ public class Part2 {
 		for(int i=0;i<a.length();i++) {
 			result=checkWhoWon(a.charAt(i),b.charAt(i));
 			if (result==1) counta++;
-			if(result==2) countb++;		}
+			if(result==-1) countb++;		}
 		return Math.abs(counta-countb);
 	}
 }
